e85b9952f66776c6ef81b40b0fd511e793b86a78
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-21 21:06:19 +0300

    added WeatherData type

---
+++ src/Types.ts
@@ -0,0 +1,32 @@
+export interface WeatherData {
+  date: String;
+  temperature: number;
+  feelsLike: number;
+  weather: {
+    main:
+      | "Clouds"
+      | "Clear"
+      | "Rain"
+      | "Snow"
+      | "Drizzle"
+      | "Thunderstorm"
+      | "Mist"
+      | "Haze"; // Main weather condition
+    description: string; // Weather description (e.g., "light rain", "few clouds", "clear sky")
+  };
+  wind: {
+    direction: string; // Wind direction as a string (e.g. 'N', 'NE', 'E', etc.)
+    speed: number; // Wind speed in km/h or mph
+  };
+  humidity: number; // Humidity percentage
+  rainPercentage: number; // Rain percentage (to be calculated)
+  windDescription:
+    | "Calm"
+    | "Breeze"
+    | "Light Wind"
+    | "Strong Wind"
+    | "Gale"
+    | "Storm"; // Description of wind/weather (to be calculated)
+  cloudless: number;
+  pressure: number; // In hPa
+}
