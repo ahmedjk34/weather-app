3ea93dfef4fa222cb3193d6b50b1a4eb148d4659
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-21 22:31:34 +0300

    added the nav component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../styles/global-rules.scss";
+import Nav from "@/components/Nav/Nav";
 
 export default function RootLayout({
   children,
@@ -8,7 +9,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
