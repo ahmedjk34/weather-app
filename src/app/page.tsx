c610ac10fc65dc74e73daabb609cdb748ae677d6
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-21 21:06:24 +0300

    TEST

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
+import { extractWeatherData } from "@/lib/util";
+
+export default async function Home() {
+  console.log(await extractWeatherData());
   return <div></div>;
 }
