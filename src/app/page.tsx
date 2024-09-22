0d14472c5c4daa18972f361d609fe0451d4d10fd
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-22 19:08:22 +0300

    Got the API hooked up wit the frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+"use server";
+
 import { extractWeatherData } from "@/lib/util";
 
-export default async function Home() {
-  //console.log(await extractWeatherData());
-
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { city: string | undefined; units: string | undefined };
+}) {
+  if (!searchParams.city || !searchParams.units) {
+    throw new Error(
+      "Invalid search query values: 'city' and 'units' cannot be empty or undefined"
+    );
+  }
+  const data = await extractWeatherData(searchParams.city, searchParams.units);
   return <div></div>;
 }
