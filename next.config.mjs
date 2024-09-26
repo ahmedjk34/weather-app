527a12c91804f1c56366350aa5c0e5ade0f49e11
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-09-26 15:38:36 +0300

    Preparing for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/weather-app",
-  output: "export", // <=== enables static exports
-  trailingSlash: true, // Helps with paths
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
